6df1a3d104270762cb1c38bee0a2c644c0e6d66b
alain descartes <ualaindescartes@gmail.com>
2024-07-24 01:17:18 -0600

    modified server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const path = require('path');
-const app = express();
+const app = express()
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
 
 app.use(express.static(path.join(__dirname, '../client/build')));
 
