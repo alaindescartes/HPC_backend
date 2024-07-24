44738201fd378256950af0ad6224c91633e47c25
alain descartes <ualaindescartes@gmail.com>
2024-07-24 01:28:01 -0600

    Fix CORS and handle other HTTP methods

--- server/server.js
+++ server/server.js
@@ -2,11 +2,25 @@ const express = require('express');
 const path = require('path');
 const app = express()
 const cors = require('cors');
+const helmet = require("helmet");
 
 app.use(cors());
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, '../client/build')));
+
+app.use(helmet({
+    contentSecurityPolicy: {
+        directives: {
+            defaultSrc: ["'self'"],
+            fontSrc: ["'self'", "https://hpc-web-82ffa098f14a.herokuapp.com/"],
+            styleSrc: ["'self'", "'unsafe-inline'"],
+            scriptSrc: ["'self'"],
+            imgSrc: ["'self'"]
+        }
+    }
+}));
+
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
